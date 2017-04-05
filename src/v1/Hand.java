b50e4f90e8a3ef78fc1b0219d84dcc164cfd2682
MichaelGinn <mikeginn13@gmail.com>
2017-04-05 17:57:47 -0400

    Updated Hand class.

--- src/v1/Hand.java
+++ src/v1/Hand.java
@@ -9,14 +9,102 @@ import java.util.ArrayList;
  */
 public class Hand {
     
-    private ArrayList <Card> hand;
+    private ArrayList <Card> hand; //player's hand is an ArrayList of cards
+    private int handSize;  //number of cards in hand
+    private int handScore; //score of card values in hand
+    private final int BLACKJACK_VALUE = 21;
     
+   /**
+    * Hand constructor creates a new hand
+    */
   public Hand(){
       hand = new ArrayList<Card>();
   }
   
+  /**
+   * Hand constructor creates hand
+   * from ArrayList of Cards
+   * @param hand
+   */
   public Hand(ArrayList <Card> hand){
       this.hand = hand;
+      for(Card c : hand){
+	  handSize++;
+	  handScore += c.getValue();
+      }
+  }
+  
+  /**
+   * returns number of cards in hand
+   * @return
+   */
+  public int getHandSize(){
+      return handSize;
+     }
+  
+  /**
+   * sets handSize
+   * @param handSize
+   */
+  public void setHandSize(int handSize){
+      this.handSize = handSize;
+  }
+  
+  /**
+   * returns score in hand
+   * @return
+   */
+  public int getHandScore(){
+      return handScore;
+  }
+  
+  /**
+   * sets handScore
+   * @param handScore
+   */
+  public void setHandScore(int handScore){
+      this.handScore = handScore;
+  }
+  
+  /**
+   * adds a card to the hand
+   * @param c
+   */
+  public void addCard(Card c){
+      hand.add(c);
+      handSize++;
+      handScore += c.getValue();
+  }
+  
+  /**
+   * checks if hand is empty
+   * @return
+   */
+  public boolean isEmpty(){
+      return handSize == 0;
+  }
+  
+  /**
+   * checks for player bust
+   * @return
+   */
+  public boolean isBust(){
+      return handScore > BLACKJACK_VALUE;
+  }
+  
+  /**
+   * displays cards in hand
+   */
+  @Override
+  public String toString(){
+      String handString = "";
+      if(isEmpty()){
+	  return "No cards in hand";
+      }
+      for(Card c : hand){
+	  handString += c + " ";
+      }
+      return handString;
   }
 
 }
