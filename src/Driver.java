092023a7f069114dcf1fa42257563d95a1a61192
Frankie <frankieFasola@yahoo.com>
2017-04-12 14:46:03 -0400

    AIPlayer started

---
+++ src/Driver.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package v1;
+
+/**
+ * @author Frankie Fasola
+ *
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+	GameLogic gl = new GameLogic();
+	gl.update();
+    }
+}
