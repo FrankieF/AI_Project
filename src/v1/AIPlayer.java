042e5247b36d7c1ae1d08dcaf8224c0bfbf11151
Spencer-E <escalants0@students.rowan.edu>
2017-04-18 16:32:53 -0400

    Update AIPlayer.java

--- src/v1/AIPlayer.java
+++ src/v1/AIPlayer.java
@@ -132,7 +132,11 @@ public double getChanceOfWinning(int scoreNeeded) {
     
     public int highBet(int count) {
     int ammntToBet;
-    	if(count >= 5)
+    	if(count >= 15)
+    	{
+    	ammntToBet = Dealer.getDealer().MIN_BET * 3;
+    	}
+    	else if(count >= 5)
     	{
     	    ammntToBet = Dealer.getDealer().MIN_BET * 2;
     	}
